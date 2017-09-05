0588d9676c0b493cd464ebc706b2ebc7a34ed179
Matt Hannan <matthannan1@gmail.com>
2017-09-05 15:50:45 -0400

    First two defs done

--- a1.py
+++ a1.py
@@ -13,7 +13,7 @@ def seconds_difference(time_1, time_2):
     >>> seconds_difference(1800.0, 1800.0)
     0.0
     """
-    
+    return time_2 - time_1    
 
 
 def hours_difference(time_1, time_2):
@@ -31,6 +31,7 @@ def hours_difference(time_1, time_2):
     >>> hours_difference(1800.0, 1800.0)
     0.0
     """
+    return seconds_difference(time_1, time_2)/3600
 
 
 
